7ecf89dcc5ba76d2b19b3dee9af09636f4260f82
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-21 00:33:54 +0900

    CSS in Javascript 4

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const h1 = document.querySelector(".hello h1")
 console.dir(h1);
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
