b4cdb8826c573eef697ecff97ddf7c2659f00298
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-22 19:19:18 +0900

    Local Storage 2

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ const loginInput = document.querySelector(".login-form Input");
 const loginForm = document.querySelector(".login-form");
 const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
     const username = loginInput.value;
     event.preventDefault(); // 브라우저의 기본 동작을 방어
-    console.log(username);
-    loginForm.classList.add("hidden");
-    greeting.classList.remove("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintUsername(username);
     // greeting.innerText = "Hello, " + username;
-    greeting.innerText = `Hello, ${username}`;
-    localStorage.setItem("username", username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintUsername(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello, ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintUsername(savedUsername);
+}
