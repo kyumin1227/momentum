0b2d2937907a4635c19281da63326415ecc9c96c
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-22 19:33:30 +0900

    Fix

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    const username = loginInput.value;
     event.preventDefault(); // 브라우저의 기본 동작을 방어
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem(USERNAME_KEY, username);
-    paintUsername(username);
-    // greeting.innerText = "Hello, " + username;
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintUsername();
 }
 
-function paintUsername(username) {
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintUsername() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello, ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -25,5 +24,5 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintUsername(savedUsername);
+    paintUsername();
 }
